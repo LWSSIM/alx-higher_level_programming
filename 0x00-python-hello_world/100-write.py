4c31f7c96d86c50f768d7c773e9b8189d23ad60e
LWSSIM <wassim.elmnajja2@gmail.com>
2023-10-30 17:52:59 +0100

    Pycodestyling

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import sys #imports the sys module
-STR ="and that piece of art is useful - Dora Korpar, 2015-10-19"
+import sys  # imports the sys module
+STR = "and that piece of art is useful - Dora Korpar, 2015-10-19"
 sys.stderr.write(STR + '\n')
 sys.exit(1)
