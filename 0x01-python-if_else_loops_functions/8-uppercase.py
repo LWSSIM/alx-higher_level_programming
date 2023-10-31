b65ee9ea1c2669500ac3919fdc253e7367d2a445
LWSSIM <wassim.elmnajja2@gmail.com>
2023-10-31 20:56:50 +0100

    Pycodestyle validation

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in str:
-        print("{}".format(chr(ord(i) - 32) if
-            (97 <= ord(i) <= 122) else i), end='')
+        print("{}".format(chr(ord(i) - 32)
+                          if (97 <= ord(i) <= 122) else i), end='')
     print('')
