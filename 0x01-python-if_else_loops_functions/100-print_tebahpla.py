0ff1bf5dd7d37102af879bb30f468cf7a5c99a6d
LWSSIM <wassim.elmnajja2@gmail.com>
2023-11-01 01:49:58 +0100

    .format

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for i in range(122, 96, -1):
-    print((chr(i - 32)) if (i % 2 != 0) else chr(i), end='')
+    print("{}".format(chr(i - 32)) if (i % 2 != 0) else chr(i), end='')
