e03d2aa70c1d9e2608d9ea62266c9d5e1e367f16
LWSSIM <wassim.elmnajja2@gmail.com>
2023-11-02 18:12:38 +0100

    ryh

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 # Prints defined names in hidden_4.pyc(3.8.x)
+
 if __name__ == "__main__":
     import hidden_4
     names = dir(hidden_4)
     for str in names:
-        if str[2:] != "__":
+        if str[:2] != "__":
             print(str)
