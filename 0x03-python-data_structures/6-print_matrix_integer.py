d0ec0a0d969a872eb559109bc353b7881906fd57
LWSSIM <wassim.elmnajja2@gmail.com>
2023-11-04 17:42:02 +0100

    fixed empty lists

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 # prints a matrix of ints
 def print_matrix_integer(matrix=[[]]):
-    if matrix:
-        for item in matrix:
-            for i in item:
-                if i != item[-1]:
-                    print('{:d}'.format(i), end=" ")
-                else:
-                    print('{:d}'.format(i))
-    else:
+    if matrix == [[]]:
         print()
+    for item in matrix:
+        for i in item:
+            if i != item[-1]:
+                print('{:d}'.format(i), end=" ")
+            else:
+                print('{:d}'.format(i))
