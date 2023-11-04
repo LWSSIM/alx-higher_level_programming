3a7274b5ec6180e75ffba2040ea25dcb07ab1bc5
LWSSIM <wassim.elmnajja2@gmail.com>
2023-11-04 15:17:25 +0100

    added edge

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 # prints list items in reverse order
 def print_reversed_list_integer(my_list=[]):
-    for item in reversed(my_list):
-        print("{:d}".format(item))
+    if my_list:
+        for item in reversed(my_list):
+            print("{:d}".format(item))
