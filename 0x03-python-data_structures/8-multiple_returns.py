5164bced6ba67d4ef469c0f058de086393e4d9e9
LWSSIM <wassim.elmnajja2@gmail.com>
2023-11-04 18:52:07 +0100

    edge caes

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # returns tuple(str.len, str.1stchar)
 def multiple_returns(sentence):
-    char1st = sentence[0] if sentence[0] else None
+    char1st = None if len(sentence) == 0 else sentence[0]
     mul = (len(sentence), char1st)
     return (mul)
