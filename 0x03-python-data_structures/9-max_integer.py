c78cc6ab3638244806fee3b2e494778473a42357
LWSSIM <127129101+LWSSIM@users.noreply.github.com>
2023-11-04 20:34:37 +0100

    Update 9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,10 @@
-#!/usr/bin/python3
-# returns the biggest int value of list
+#!/usr/bin/python3 
+# returns the biggest int value of list 
 def max_integer(my_list=[]):
     if len(my_list) == 0:
-        return (None)
-    return max(my_list)
+      return (None)
+    largest = my_list[0]
+    for n in my_list:
+      if n > largest:
+        largest = n
+    return n
