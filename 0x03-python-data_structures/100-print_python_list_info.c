952e437f1f970f5d8d1a63e48ea840266040f86e
LWSSIM <wassim.elmnajja2@gmail.com>
2023-11-05 01:22:52 +0100

    100 -v2

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <Python.h>
+#include <stdio.h>
 
 /**
  * print_python_list_info - prints basics info about Python lists
@@ -7,13 +7,13 @@
  */
 void print_python_list_info(PyObject *p)
 {
-	PyObject *item;
-	Py_ssize_t i;
+	PyObject *item, *item_str;
+	Py_ssize_t i, list_size, num_allocated;
 
 	if (Pylist_check(p))
 	{
-		Py_ssize_t list_size = PyList_Size(p);
-		Py_ssize_t num_allocated = PyList_GET_SIZE(p);
+		list_size = PyList_Size(p);
+		num_allocated = PyList_GET_SIZE(p);
 
 		printf("[*] Size of the Python List = %zd\n", list_size);
 		printf("[*] Allocated = %zd\n", num_allocated);
@@ -24,7 +24,7 @@ void print_python_list_info(PyObject *p)
 			printf("Element %zd: ", i);
 			if (item)
 			{
-				PyObject *item_str = PyObject_Str(item);
+				item_str = PyObject_Str(item);
 
 				printf("%s\n", item_str);
 			}
