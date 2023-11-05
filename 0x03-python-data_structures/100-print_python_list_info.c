6be4a4579514ed2a3d5182cdb09cb9cd1a6a9494
LWSSIM <wassim.elmnajja2@gmail.com>
2023-11-05 15:53:56 +0100

    100 -v-4

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -7,33 +7,18 @@
  */
 void print_python_list_info(PyObject *p)
 {
-	Py_ssize_t i, list_size, num_allocated;
-	PyObject *item, *item_str;
-	char *str;
+	int size, allocated, i;
+	PyObject *item;
 
-	if (PyList_Check(p))
+	size = PyList_Size(p);
+	allocated = ((PyListObject *)p)->allocated;
+
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %d\n", allocated);
+
+	for (i = 0; i < size; i++)
 	{
-		list_size = PyList_Size(p);
-		num_allocated = PyList_GET_SIZE(p);
-
-		printf("[*] Size of the Python List = %zd\n", list_size);
-		printf("[*] Allocated = %zd\n", num_allocated);
-
-		for (i = 0; i < list_size; i++)
-		{
-			item = PyList_GetItem(p, i);
-			printf("Element %zd: ", i);
-			if (item)
-			{
-				item_str = PyObject_Str(item);
-				if (item_str)
-				{
-					str = (char *)PyUnicode_AsUTF8(item_str);
-					printf("%s\n", str);
-					free(item_str);
-					free(str);
-				}
-			}
-		}
+		item = PyList_GetItem(p, i);
+		printf("Element %d: %s\n", i, Py_TYPE(item)->tp_name);
 	}
 }
