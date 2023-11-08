2b65027323ba6dfc5acae4d6f15c27dcb5517101
LWSSIM <wassim.elmnajja2@gmail.com>
2023-11-08 16:13:25 +0100

    added 1-search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# ...replaces all occurrences of an element by another in a new list.
+def search_replace(my_list, search, replace):
+    new_list = [] 
+    for i in my_list:
+        if i == search:
+            i = replace
+        new_list.append(i)
+    return new_list
