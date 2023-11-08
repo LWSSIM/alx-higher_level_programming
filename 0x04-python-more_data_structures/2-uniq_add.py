61b79c135854131fd85fdc624b2bc8824909261e
LWSSIM <wassim.elmnajja2@gmail.com>
2023-11-08 16:38:26 +0100

    uniq uniq_add

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,5 +2,5 @@
 # ...adds all unique integers in a list (only once for each integer).
 def uniq_add(my_list=[]):
     new_set = set(my_list)
-    res = sum(i for i in new_set) 
+    res = sum(i for i in new_set)
     return res
