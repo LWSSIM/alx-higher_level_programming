2353154fb16a6b39ea0b54fdab597eedf0ebf0ec
LWSSIM <wassim.elmnajja2@gmail.com>
2023-11-09 00:49:44 +0100

    fix -1 103

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,23 +1,49 @@
-#include "Python"
+#include "Python.h"
 #include "stdio.h"
 
 /**
-* print_python_list - print list items
-* @p: pointer to list
-*/
+ * print_python_bytes - print byts info
+ * @p: ptr tp list
+ */
+void print_python_bytes(PyObject *p)
+{
+	Py_ssize_t size = Py_SIZE(p), i;
+	char *data = PyBytes_AS_STRING(p);
+
+	printf("[.] bytes object info\n");
+	if (!PyBytes_Check(p))
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+		return;
+	}
+	printf("  size: %ld\n", size);
+	printf("  trying string: %s\n", data);
+	printf("  first 6 bytes: ");
+	for (i = 0; i < size && i < 6; i++)
+	{
+		printf("%02x ", data[i] & 0xFF);
+	}
+	printf("\n");
+}
+
+/**
+ * print_python_list - print list items
+ * @p: pointer to list
+ */
 void print_python_list(PyObject *p)
 {
 	PyListObject *list = (PyListObject *)p;
-	Py_ssize_t size = PyList_GET_SIZE(p);
+	PyObject *item;
+	Py_ssize_t size = PyList_GET_SIZE(p), i;
 	Py_ssize_t allocated = list->allocated;
 
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %ld\n", size);
 	printf("[*] Allocated = %ld\n", allocated);
 
-	for (Py_ssize_t i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
-		PyObject *item = list->ob_item[i];
+		item = list->ob_item[i];
 
 		printf("Element %ld: %s\n", i, PyBytes_Check(item) ? "bytes" : "unknown");
 		if (PyBytes_Check(item))
@@ -25,24 +51,4 @@ void print_python_list(PyObject *p)
 			print_python_bytes(item);
 		}
 	}
-}
-/**
-* print_python_bytes - print byts info
-* @p: ptr tp list
-*/
-void print_python_bytes(PyObject *p)
-{
-	PyBytesObject *bytes = (PyBytesObject *)p;
-	Py_ssize_t size = Py_SIZE(p);
-	char *data = PyBytes_AS_STRING(p);
-
-	printf("[.] bytes object info\n");
-	printf("  size: %ld\n", size);
-	printf("  trying string: %s\n", data);
-	printf("  first 6 bytes: ");
-	for (Py_ssize_t i = 0; i < size && i < 6; i++)
-	{
-		printf("%02x ", data[i] & 0xFF);
-	}
-	printf("\n");
 }
