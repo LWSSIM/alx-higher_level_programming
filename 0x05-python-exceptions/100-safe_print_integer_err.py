e47a38892fcf906f538faf32c18058abdb89ead0
LWSSIM <wassim.elmnajja2@gmail.com>
2023-11-20 18:31:27 +0100

    updated exception range

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,6 +7,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as ve:
+    except Exception as ve:
         print("Exception: {}".format(ve), file=sys.stderr)
         return False
