fa16152602e5eb0e079a12bdc628aca2f581ac60
LWSSIM <wassim.elmnajja2@gmail.com>
2023-11-20 18:27:17 +0100

    added 100-safe print err

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# print an int with error msg exceptions
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError as ve:
+        print("Exception: {}".format(ve), file=sys.stderr)
+        return False
