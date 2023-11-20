dcb437884adff103b51ecdb6412ab12f4c8374f8
LWSSIM <wassim.elmnajja2@gmail.com>
2023-11-20 14:37:12 +0100

    added 0-safe_print_list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# prints x elements of a list.
+def safe_print_list(my_list=[], x=0):
+	n = 0
+	for i in range(x):
+		try:
+			print("{}".format(my_list[i]), end='')
+			n += 1
+		except IndexError:
+			continue
+	print()
+	return n
