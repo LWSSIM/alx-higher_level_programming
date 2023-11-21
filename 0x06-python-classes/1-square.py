e6c28e212d51f815eaf0b5ad1b54f77f76c6aa13
LWSSIM <wassim.elmnajja2@gmail.com>
2023-11-21 17:20:38 +0100

    added 1-s

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Module:
+    create class Square with attributes
+"""
+
+
+class Square:
+    """
+    class:
+        Square
+
+    Note:
+        this class has no class attributes
+    Args:
+        size of the square <-(int)
+    Methods:
+        __init__: init the class atrbt
+    """
+
+    def __init__(self, size=0):
+        """
+        Init an instance of class Square
+
+        Args:
+            input new square size(private instance attribute)
+        """
+        self._Square__size = size
