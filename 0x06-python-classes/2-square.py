e850d9beb633f1bb34193bda4fa737dadf7e48c6
LWSSIM <wassim.elmnajja2@gmail.com>
2023-11-21 18:16:23 +0100

    added 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+"""
+Module: create class Square with attributes
+"""
+
+
+class Square:
+    """
+    class:
+        Square
+
+    Note:
+        this class has no class attributes
+    Args:
+        size (int): of the square
+    Methods:
+        __init__: init the class atrbt
+    """
+
+    def __init__(self, size=0):
+        """
+        Init an instance of class Square raise some exeptions
+        with msg
+
+        Args:
+            input new square size(private instance attribute)
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be an integer")
+        else:
+            self._Square__size = size
