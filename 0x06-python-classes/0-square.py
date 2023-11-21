84eaec265a10f993d5f81a26832f529e569f8e07
LWSSIM <wassim.elmnajja2@gmail.com>
2023-11-21 16:56:16 +0100

    added 0-square.c

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# class Square
+class Square:
+    '''
+    class: 
+        Square
+    Note:
+        empty test class used with 0-main.py
+    Attributes:
+        None
+    '''
+    pass
