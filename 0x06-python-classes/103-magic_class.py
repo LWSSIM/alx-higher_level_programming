d188379715f9c7487e9a9647f5240cc966d61541
LWSSIM <wassim.elmnajja2@gmail.com>
2023-11-24 15:50:58 +0100

    small fixes

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -17,13 +17,19 @@ class MagicClass:
     """
 
     def __init__(self, radius=0):
-        if type(radius) is not int and type(radius) is not float:
+        if not isinstance(radius, int) and not isinstance(radius, float):
             raise TypeError("radius must be a number")
         else:
             self.__radius = radius
 
     def area(self):
+        """
+        Calculates the area of a circle
+        """
         return self.__radius**2 * math.pi
 
     def circumference(self):
+        """
+        Calculates the circumference of a circle
+        """
         return self.__radius * 2 * math.pi
--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -62,7 +62,7 @@ class Square:
         """
         if type(size) is not int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
