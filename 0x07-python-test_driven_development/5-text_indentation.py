e3459ff39e93a604b839beed07f7e240f2e31326
LWSSIM <wassim.elmnajja2@gmail.com>
2023-11-27 01:43:10 +0100

    added more detailed docstring>5-*.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -6,13 +6,21 @@
 
 def text_indentation(text):
     '''
-    formats input text with 2*'\n' per sentence
+    formats and print input text with 2*'\n' per sentence
 
     Args:
         text: input
 
     Raises:
         on bad format raises type err
+
+    Note:
+        - Delimiters define sentences, prints 1 sentence/line /
+            Prints newline after each line
+
+        leading / trailling spaces in each line /
+        are ommited.
+        recursive ' ' inside a line is replcaed by a single ' '
     '''
     if not isinstance(text, str) or text is None:
         raise TypeError('text must be a string')
