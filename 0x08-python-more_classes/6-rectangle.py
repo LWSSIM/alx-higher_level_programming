cff56f64640328dcfee47d5423093ea188d55c70
LWSSIM <wassim.elmnajja2@gmail.com>
2023-11-27 21:19:30 +0100

    added 6-.py

---
+++ 0x08-python-more_classes/6-rectangle.py
@@ -0,0 +1,131 @@
+#!/usr/bin/python3
+"""Class Rectangle
+"""
+
+
+class Rectangle:
+    """Defines a rectangle
+
+        Attributes:
+            width: int
+            height: int
+            number_of_instances: int
+
+        Properties:
+            def width(self): setter + getter
+            def height(self): setter + getter
+    """
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        """initialize Rectangle
+
+        Args:
+            width: input rectangle width (optional)
+            height: input rectangle height (optional)
+                both args are defaulted to 0
+        """
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        """Getter for the width attr
+
+            Return:
+                width private attr
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter for width
+
+            Args:
+                value: desired width
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter for the height attr
+
+            Return:
+                height private attr
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter for height
+
+            Args:
+                value: desired height
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
+
+    def area(self):
+        """Method for area calculation
+
+            Returns:
+                rectangle area->int
+        """
+        return self.height * self.width
+
+    def perimeter(self):
+        """Method for perimeter calculation
+
+            Returns:
+                rectagle perileter->int
+        """
+        if self.height == 0 or self.width == 0:
+            return 0
+
+        return 2 * (self.height + self.width)
+
+    def __str__(self):
+        '''str method of the Class
+
+            Return:
+                string representing the attributes
+                    using the '#' char
+
+            Note:
+                retuns None is attrs are 0
+        '''
+        if self.height == 0 or self.width == 0:
+            return ''
+
+        width = self.width
+        _print = (('#' * width) + '\n') * self.height
+        return _print[:-1]
+
+    def __repr__(self):
+        '''repr method of the Class
+
+            Retun:
+                expression that can be used by eval
+                that can be used to recreate an identical
+                instance
+        '''
+        return 'Rectangle({}, {})'.format(self.width, self.height)
+
+    def __del__(self):
+        '''del method for the class
+
+            Note:
+                Prints a certain msg upon deleting the class
+        '''
+        Rectangle.number_of_instances -= 1
+        print('Bye rectangle...')
---
+++ 0x08-python-more_classes/tests/6-main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+import sys
+sys.path.insert(0, '..')
+
+Rectangle = __import__('6-rectangle').Rectangle
+
+my_rectangle_1 = Rectangle(2, 4)
+my_rectangle_2 = Rectangle(2, 4)
+print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
+del my_rectangle_1
+print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
+del my_rectangle_2
+print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
+
