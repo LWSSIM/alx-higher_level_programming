2ef8b56a359473c6f34df15706b53ff780cd395a
LWSSIM <wassim.elmnajja2@gmail.com>
2023-12-04 18:32:52 +0100

    added 5-*

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module: Empty class
+"""
+
+
+class BaseGeometry:
+    """
+    class:
+
+    Note:
+        this class has no class attributes
+
+    Args:
+        arg1 (type):
+
+    Methods:
+        __init__: init the class attributes
+    """
+
+    pass
---
+++ 0x0A-python-inheritance/tests/5-main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import sys
+
+sys.path.insert(0, "..")
+
+BaseGeometry = __import__("5-base_geometry").BaseGeometry
+
+bg = BaseGeometry()
+
+print(bg)
+print(dir(bg))
+print(dir(BaseGeometry))
