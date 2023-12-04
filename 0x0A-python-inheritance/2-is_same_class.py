b790802b38fd622580b0b6615b194888256ec98e
LWSSIM <wassim.elmnajja2@gmail.com>
2023-12-04 18:09:07 +0100

    added 2-*

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module: is this an instance of a class?
+"""
+
+
+def is_same_class(obj, a_class):
+    """is the object is exactly an instance of the
+    specified class
+
+    Args:
+        obj: checked object.
+        a_class: specified class.
+
+    Returns:
+       bool: The return value. True for success, False otherwise.
+    """
+    return type(obj) == a_class
