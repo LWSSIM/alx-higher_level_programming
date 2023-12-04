023318257eb5a9912eb3ddf82dfb7b9be5f3107b
LWSSIM <wassim.elmnajja2@gmail.com>
2023-12-04 20:34:59 +0100

    fix

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -29,7 +29,7 @@ class BaseGeometry:
            Type:
            Value:
         """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
