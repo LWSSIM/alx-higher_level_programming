14c58392de9daa9416c88be69ba55ef2179aef30
LWSSIM <wassim.elmnajja2@gmail.com>
2023-12-05 16:55:20 +0100

    added 8-*

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module: function that returns the dictionary description with simple data structure
+"""
+
+
+def class_to_json(obj):
+    """get class dict descritpion
+    Note:
+        for JSON serialization of obj
+
+    Args:
+        my_obj: passed obj isinstance of a class
+
+    Return:
+        returns the dictionary description with simple data structure
+        i.e: __dict__ method
+    """
+    return obj.__dict__
