f22858950da9388abef1187afb8bac302536873c
LWSSIM <wassim.elmnajja2@gmail.com>
2023-12-06 00:40:17 +0100

    added 100-apend

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -14,9 +14,8 @@ def append_after(filename="", search_string="", new_string=""):
     """
     with open(filename, "r") as f:
         lines = f.readlines()
-
     with open(filename, "w") as f:
         for line in lines:
             f.write(line)
             if search_string in line:
-                f.write(search_string)
+                f.write(new_string)
