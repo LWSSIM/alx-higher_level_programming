3b5e33b24cf2f7b4cb1bf7e7f38cc425f02dd1a5
LWSSIM <127129101+LWSSIM@users.noreply.github.com>
2023-12-05 23:02:32 +0100

    Update 100-append_after.pyjgf

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -15,8 +15,9 @@ def append_after(filename="", search_string="", new_string=""):
     with open(filename, 'r') as f:
         lines = f.readlines()
 
-
-    for line in lines:
-        if search_string in line:
-            with open(filename, "w") as f:
+    
+    with open(filename, "w") as f:
+        for line in lines:
+            f.write(line)
+            if search_string in line:
                 f.write(search_string)
