62b3f8aee426ebd9d6ccf841b85801ec871626b3
LWSSIM <wassim.elmnajja2@gmail.com>
2023-12-05 16:21:50 +0100

    added 6-*

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module:function that creates an Object from a “JSON file”
+"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    fetch py_obj from JSON str format
+
+    Args:
+        filename:
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(f)
