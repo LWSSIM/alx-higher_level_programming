a5f6054ab7fb8c6df8fdce5159c49369c60ee3b2
LWSSIM <wassim.elmnajja2@gmail.com>
2024-01-17 00:48:32 +0100

    added 101-*

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,7 @@
+-- Script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending).
+-- Imported a table dump in hbtn_0c_0 database from temperature.sql
+SELECT city, AVG(value) AS avg_temp 
+FROM temperatures 
+GROUP BY city 
+ORDER BY avg_temp DESC;
+
