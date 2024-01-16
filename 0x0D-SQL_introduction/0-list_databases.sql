a4a9ab166b75e75d256e2385ef479769c53659ca
LWSSIM <wassim.elmnajja2@gmail.com>
2024-01-16 22:52:57 +0100

    added 1-*

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,2 +1,3 @@
 -- Lists all databases of your MySQL server.
 SHOW DATABASES;
+
---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+--  Creates the database hbtn_0c_0 in a MySQL server, If the database hbtn_0c_0 already exists, the script should not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+
