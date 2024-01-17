711aa509290e12a8d72ede90fa6f5a586fc8f8e5
LWSSIM <wassim.elmnajja2@gmail.com>
2024-01-17 01:07:12 +0100

    q fix

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,6 @@
 -- Script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending).
 -- Imported a table dump in hbtn_0c_0 database from temperature.sql
+-- 'mysql -u your_username -p your_database < your_dump_file.sql'
 SELECT city, AVG(value) AS avg_temp 
 FROM temperatures 
 GROUP BY city 
--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,8 +1,9 @@
 -- script that displays the top 3 of cities temperature during July and August ordered by temperature (descending)
 -- Imported table dump in hbtn_0c_0 database from temperature.sql
-SELECT city, AVG(value) AS avg_tmp
+SELECT city, AVG(value) AS avg_temp
   FROM temperatures
   WHERE month = 7 OR month = 8
   GROUP BY city
-  ORDER BY avg_tmp DESC
+  ORDER BY avg_temp DESC
   LIMIT 3;
+
