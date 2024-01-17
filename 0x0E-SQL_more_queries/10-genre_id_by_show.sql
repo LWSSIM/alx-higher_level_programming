4875147f045cde2a87522fd30082c04609f67d5b
LWSSIM <wassim.elmnajja2@gmail.com>
2024-01-18 00:27:23 +0100

    added 10-*

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,8 @@
+-- Import the database dump from hbtn_0d_tvshows
+--
+-- Write a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+--
+--  Each record should display: tv_shows.title - tv_show_genres.genre_id
+--  Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+--  You can use only one SELECT statement
+--  The database name will be passed as an argument of the mysql command
