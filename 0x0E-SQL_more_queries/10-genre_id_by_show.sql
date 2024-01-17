5e24921bf61ded536e6651ce39de9f130a7ad62c
LWSSIM <wassim.elmnajja2@gmail.com>
2024-01-18 00:29:05 +0100

    fix2

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -6,3 +6,8 @@
 --  Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 --  You can use only one SELECT statement
 --  The database name will be passed as an argument of the mysql command
+SELECT tv_shows.title, tv_show_genres.genre_id
+  FROM tv_shows
+  JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+  ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+
