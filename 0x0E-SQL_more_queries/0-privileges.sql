563e41b580477ae94c9418375aa47fac0bce8f72
LWSSIM <wassim.elmnajja2@gmail.com>
2024-01-17 23:19:57 +0100

    added the things

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,6 @@
 -- Lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
+-- Users might not exists
+
 -- List privileges for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
