caae842f6b1c2bcc96e32bcaad1b4f3f53bf37fd
LWSSIM <wassim.elmnajja2@gmail.com>
2024-02-12 17:04:05 +0100

    semi stndrd fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,8 @@
 #!/usr/bin/node
 /*  prints a message depending of the number of arguments passed */
 
-let argc = process.argv.length - 2;
+const argc = process.argv.length - 2;
 
-let msg1 = 'No argument', msg2 = 'Argument found', msg3 = 'Arguments found';
+const msg1 = 'No argument'; const msg2 = 'Argument found'; const msg3 = 'Arguments found';
 
-if (argc == 0)
-  console.log(msg1);
-else if (argc == 1)
-  console.log(msg2);
-else
-  console.log(msg3);
+if (argc === 0) { console.log(msg1); } else if (argc === 1) { console.log(msg2); } else { console.log(msg3); }
