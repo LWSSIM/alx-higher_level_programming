7918917832cbaeb6eaa6f8ee7ac30d6aa9dc7d96
LWSSIM <wassim.elmnajja2@gmail.com>
2024-02-12 17:49:25 +0100

    added 6-*

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* print 3 lines using array + loop */
+
+const firstL = 'C is fun'; const secondL = 'Python is cool'; const thirdL = 'JavaScript is amazing';
+const strARR = [firstL, secondL, thirdL];
+
+for (let i = 0; i < strARR.length; i++) {
+  console.log(strARR[i]);
+}
