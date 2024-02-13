54b626f235a8dd7d758631ae15ab8cdbd7d0feed
LWSSIM <wassim.elmnajja2@gmail.com>
2024-02-13 19:09:15 +0100

    added 10-*

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 /* prints the number of arguments already printed and the new argument value */
 /* Output format: <number arguments already printed>: <current argument value> */
+/* Jiboli LAGMI */
 
 let i = 0;
 exports.logMe = function (item) {
