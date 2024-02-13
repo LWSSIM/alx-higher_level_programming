beb688a74782bfc242ff0ed4dae6c471ed9e66be
LWSSIM <wassim.elmnajja2@gmail.com>
2024-02-13 16:32:04 +0100

    added 3-*

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -2,6 +2,7 @@
 /* Class that defines a rectangle */
 /* Add and init 2 attrs (w & h) */
 /* Add attr validation */
+/* Add print instance method */
 
 class Rectangle {
   constructor (w, h) {
@@ -10,6 +11,16 @@ class Rectangle {
     }
     this.width = w;
     this.height = h;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let arr = '';
+      for (let j = 0; j < this.width; j++) {
+        arr += 'X';
+      }
+      console.log(arr);
+    }
   }
 }
 
