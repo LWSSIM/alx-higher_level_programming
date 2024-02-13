59b63e48c93733c10fca99ff6a5392ca59975659
LWSSIM <wassim.elmnajja2@gmail.com>
2024-02-13 16:58:21 +0100

    added6-*

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+/* Class that defines a square inherit from rectangle */
+/* uses extends + super() */
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (!c) {
+      c = 'X';
+    }
+    for (let index = 0; index < this.height; index++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
