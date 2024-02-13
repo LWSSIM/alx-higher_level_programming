daa993597d98d65e1644025e8fec4daad393e27f
LWSSIM <wassim.elmnajja2@gmail.com>
2024-02-13 16:13:30 +0100

    added 1-*

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/* Class that defines a rectangle */
+/* Add and init 2 attrs (w & h) */
+
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
