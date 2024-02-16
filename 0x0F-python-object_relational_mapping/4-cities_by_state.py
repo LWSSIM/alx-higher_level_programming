a81d44533f4cc70d5c0a63ad272ae8a515189bc4
LWSSIM <wassim.elmnajja2@gmail.com>
2024-02-16 17:29:09 +0100

    added 4-*

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+Write a script that lists all cities from the database hbtn_0e_4_usa
+
+ -Your script should take 3 arguments:
+  mysql username, mysql password and database name
+ -You must use the module MySQLdb (import MySQLdb)
+ -Your script should connect to a MySQL server running
+  on localhost at port 3306
+ -Results must be sorted in ascending order by cities.id
+ -You can use only execute() once
+ -Results must be displayed as they are in the example below
+ -Your code should not be executed when imported
+"""
+
+
+import MySQLdb
+import sys
+
+
+def list_all_cities(username, password, database):
+    try:
+        conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database,
+            charset="utf8",
+        )
+        cursor = conn.cursor()
+
+        cursor.execute(
+            "SELECT cities.id, cities.name, states.name \
+            FROM cities \
+            INNER JOIN states ON cities.state_id=states.id \
+            ORDER BY cities.id"
+        )
+
+        rows = cursor.fetchall()
+
+        for row in rows:
+            print(row)
+
+        cursor.close()
+        conn.close()
+    except MySQLdb.Error as e:
+        print("MySQLdb.Error: ", e)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: script.py <un> <ps> <db>")
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    list_all_cities(username, password, database)
