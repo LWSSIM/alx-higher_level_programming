4adc026d7f7f320e16928afabf6754cf65134fd6
LWSSIM <wassim.elmnajja2@gmail.com>
2024-02-29 18:20:08 +0100

    added 0-*

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Takes in a URL, sends a request to that URL, and displays the size of the body of the response
+# -The size must be displayed in bytes
+URL="$1"
+curl -sI "$1" | grep Content-Length | cut -d ' ' -f2
