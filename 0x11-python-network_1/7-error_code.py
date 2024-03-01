59794faef8cdabc426a59f8d0f57502fc2ce7be1
LWSSIM <wassim.elmnajja2@gmail.com>
2024-03-02 00:42:56 +0100

    added 7-*

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Takes in a URL, sends a request to the URL
+    and displays the body of the response.
+"""
+
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+
+    response = requests.get(url)
+
+    if response.status_code >= 400:
+        print("Error code:", response.status_code)
+    else:
+        print(response.text)
