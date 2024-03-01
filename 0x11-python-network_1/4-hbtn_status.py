f99825ba40884134e16acee85f88dd4c3119285c
LWSSIM <wassim.elmnajja2@gmail.com>
2024-03-01 23:13:47 +0100

    added 4-*

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Fetches https://alx-intranet.hbtn.io/status
+"""
+
+
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        print("Body response:")
+        print("\t- type:", type(response.content))
+        print("\t- content:", response.content)
