2083e01267c39af44e8c63c1952b0c6a5be4b84e
LWSSIM <wassim.elmnajja2@gmail.com>
2024-03-02 01:33:52 +0100

    added psc

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Takes your GitHub credentials (username and password) and uses the GitHub API to display your id
+"""Takes your GitHub credentials
+(username and password) and uses the GitHub API to display your id
 """
 
 
