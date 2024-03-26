6b2f6f3dbe12fbef20b054400550f6ab04b51cc2
LWSSIM <wassim.elmnajja2@gmail.com>
2024-03-26 22:37:37 +0000

    added 4-*

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+/* prints the number of movies where the character
+ * “Wedge Antilles” is present */
+
+const request = require('request');
+const scriptName = process.argv[1].split('/').pop();
+
+if (process.argv.length !== 3) {
+  console.log(`Usage: ./${scriptName} <url>`);
+  process.exit(1);
+}
+
+const url = process.argv[2];
+
+request(url, (err, response, body) => {
+  if (err) console.log(err.message);
+
+  else if (response.statusCode !== 200) console.log(response.statusMessage);
+
+  else {
+    const data = JSON.parse(body);
+    const id = 18;
+    let count = 0;
+    data.results.forEach(film => {
+      if (film.characters.includes(
+        `https://swapi-api.alx-tools.com/api/people/${id}/`)) {
+        count++;
+      }
+    });
+    console.log(count);
+  }
+});
