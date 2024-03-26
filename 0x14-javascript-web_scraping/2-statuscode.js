6f914eaf2947948357a91216f4b9408fc7b79de7
LWSSIM <wassim.elmnajja2@gmail.com>
2024-03-26 21:43:35 +0000

    semistandard

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,11 +6,11 @@ const request = require('request');
 const scriptName = process.argv[1].split('/').pop();
 
 if (process.argv.length !== 3) {
-  console.log(`Usage: ./${scriptName} <URL>`)
-  process.exit(1)
+  console.log(`Usage: ./${scriptName} <URL>`);
+  process.exit(1);
 }
 
-const url = process.argv[2]
+const url = process.argv[2];
 
 request(url, (err, response) => {
   if (err) console.log(err.message);
