b982978c3de72e6cc9388c073fb6ee6fdc0706cd
LWSSIM <wassim.elmnajja2@gmail.com>
2024-03-26 21:42:22 +0000

    added 2- safer verssion

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,21 @@
 #!/usr/bin/node
 /* display the status code of a GET request
- * Format=> code: <status code>*/
+Format code: < status code > */
 
-const request = require('request')
+const request = require('request');
+const scriptName = process.argv[1].split('/').pop();
 
-request(process.argv[2], (response) => {
-  console.log(`code: ${response.statusCode}`)
-})
+if (process.argv.length !== 3) {
+  console.log(`Usage: ./${scriptName} <URL>`)
+  process.exit(1)
+}
+
+const url = process.argv[2]
+
+request(url, (err, response) => {
+  if (err) console.log(err.message);
+
+  else if (!response) console.log('Empty response');
+
+  else console.log(`code: ${response.statusCode}`);
+});
