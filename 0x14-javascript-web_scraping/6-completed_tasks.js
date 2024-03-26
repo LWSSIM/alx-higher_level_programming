f4b3d8738af61696756356a785d09bcd679b5a93
LWSSIM <wassim.elmnajja2@gmail.com>
2024-03-26 23:26:14 +0000

    added 6-*

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+/* compute the number of completed tasks by user id */
+
+const request = require('request');
+const scriptName = process.argv[1].split('/').pop();
+
+if (process.argv.length !== 3) {
+  console.log(`Usage: ./${scriptName} <url>`);
+  process.exit(1);
+}
+
+const url = process.argv[2];
+
+request(url, { json: true }, (err, res, body) => {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  const completedTasks = {};
+  body.forEach(task => {
+    if (task.completed === true) {
+      if (!completedTasks[task.userId]) {
+        completedTasks[task.userId] = 1;
+      } else {
+        completedTasks[task.userId]++;
+      }
+    }
+  });
+  console.log(completedTasks);
+});
